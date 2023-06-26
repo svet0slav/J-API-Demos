4df08acbbd4f4a161573b56f2d2d2297113146b3
Svetoslav Ivanov <svetoslav.ivanov@company.com>
2023-06-26 20:36:39 +0300

    UI Console application

---
+++ SimplifiedSlotMachine/SimplifiedSlotMachine.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SimplifiedSlotMachine.Console;
+using GameModel.Abstract;
+using SimplifiedSlotMachine.GameModel;
+
+Console.WriteLine("Hello, welcome to Simple Slot Machine ABP*!");
+
+var ui = new UIController();
+
+decimal initialBalance = ui.EnterBalance();
+decimal currentBalance = initialBalance;
+
+var spin = new SimplifiedSpin();
+var game = new SimplifiedGameModel(spin);
+
+while (currentBalance >= 0)
+{
+    decimal stake = ui.EnterStake(currentBalance);
+    var stageModel = new SimplifiedGameStageModel(game);
+    var firstStage = stageModel.Start(currentBalance, stake);
+    var results = game.RotateMultiple(firstStage);
+
+    ui.PrintStage(results);
+    ui.PrintTotals(stageModel, results);
+
+    currentBalance = results.Last().EndBalance;
+}
+
+Console.WriteLine("End of the game!");
+Console.ReadKey();
---
+++ SimplifiedSlotMachine/SimplifiedSlotMachine.Console/UIController.cs
@@ -0,0 +1,68 @@
+﻿using GameModel.Abstract;
+using SimplifiedSlotMachine.DataModel;
+using System;
+
+namespace SimplifiedSlotMachine.Console
+{
+    internal class UIController
+    {
+
+        public UIController() { }
+
+        public decimal EnterBalance() {
+            decimal initialBalance = 0;
+
+            while (initialBalance <= 0)
+            {
+                System.Console.WriteLine("Please, deposit money you would like to play with:");
+                var enteredbalance = System.Console.ReadLine();
+
+                if (!decimal.TryParse(enteredbalance, out initialBalance))
+                {
+                    initialBalance = 0;
+                }
+            }
+            return initialBalance;
+        }
+
+        public decimal EnterStake(decimal currentBalance)
+        {
+            decimal stake = 0;
+            while (stake > 0)
+            {
+                System.Console.WriteLine("Enter stake amount:");
+                var enteredStake = System.Console.ReadLine();
+                if (!decimal.TryParse(enteredStake, out stake) || stake <= 0.0M || stake > currentBalance)
+                {
+                    System.Console.WriteLine("Stake amount should be positive <= Current Balance:");
+                    stake = 0;
+                }
+            }
+
+            return stake;
+        }
+
+        public void PrintStage(List<Stage> stages)
+        {
+            foreach(var stage in stages)
+            {
+                var symbols = stage.SpinResult;
+                foreach(var symbol in symbols)
+                {
+                    System.Console.Write(symbol.Letter);
+                }
+
+            }
+        }
+
+        public void PrintTotals(IGameStageModel stageModel, List<Stage> stages)
+        {
+            var won = stages.Sum(s => s.WinAmount);
+            System.Console.WriteLine($"You have won: {won}");
+
+            var balance = stages.Last().EndBalance;
+            System.Console.WriteLine($"Current balance is: {balance}");
+        }
+
+    }
+}
